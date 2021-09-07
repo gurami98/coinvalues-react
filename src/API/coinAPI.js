d36c5b3788d74136032827e6ae00c422de82013a
gurami-exceed <g.chilindrishvili.exceedteam@gmail.com>
2021-09-08 00:24:35 +0400

    temp workaround for CORS error

--- src/API/coinAPI.js
+++ src/API/coinAPI.js
@@ -1,4 +1,10 @@
 import superagent from "superagent"
 const API = 'http://localhost:3003'
+const proxy = 'https://cors-anywhere.herokuapp.com'
 
-export const getAllCoins = () => superagent.get(`${API}/coin/get-all`)
+// export const getAllCoins = async () => await superagent.get(`${API}/coin/get-all`) // to use with back-end
+
+// to use straight from front-end but with proxy server - https://cors-anywhere.herokuapp.com/corsdemo - go here to activate it
+export const getAllCoins = async () => await superagent.get(`${proxy}/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`)
+                                                       .set('X-CMC_PRO_API_KEY', '6c5e97f3-9cdb-438f-8b01-77b71c8bd419')
+                                                       .set('Accept', 'application/json')
